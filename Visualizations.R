c4d89ccdf89cd929fec6f34e65af19dc05b39847
rogerjeasy <rogerjeasy@gmail.com>
2025-01-29 16:28:06 +0100

    visualization script

---
+++ Visualizations.R
@@ -0,0 +1,7 @@
+## Load packages
+library(dplyr)
+library(ggplot2)
+library(sf)          ## for function st_transform() (geo data fromat)
+library(osmdata)
+library(maptiles)    ## for get_tiles() (background map)
+library(tidyterra) 

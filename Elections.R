2997e8276c666f12f832c8887068d5fb5a911102
radejev <rade.jevdenic@stud.fhgr.ch>
2025-01-30 15:53:52 +0100

    Added 2019 results
    Added 2019 results

--- Elections.R
+++ Elections.R
@@ -7,6 +7,7 @@ library(stringr)
 import1 <- read.csv("sd-t-17.02-NRW2023-parteien-appendix.csv",
                    header = TRUE,
                    sep = ";")
+import1
 
 import2 <- read_excel("px-x-0102010000_104_20250127-155044.xlsx", 
                       skip = 2)
@@ -19,15 +20,16 @@ import5 <- read_excel("Gemeindestand.xlsx")
 
 
 ### DATASET 1: Election Results 2023
-
 election2023 <- import1 %>%
   select(gemeinde_bezeichnung,
          gemeinde_nummer,
          partei_staerke,
+         letzte_wahl_partei_staerke,
          partei_bezeichnung_de,
          partei_bezeichnung_fr) %>%   
   filter(!is.na(gemeinde_nummer) & gemeinde_nummer != "") %>% 
-  mutate(partei_staerke = round(partei_staerke, 0)) %>%
+  mutate(partei_staerke = round(partei_staerke, 0),
+         letzte_wahl_partei_staerke = round(letzte_wahl_partei_staerke, 0)) %>%
   rename(partynameDE = partei_bezeichnung_de,  
          partynameFR = partei_bezeichnung_fr,  
          municipality = gemeinde_bezeichnung,  
@@ -48,17 +50,34 @@ election2023 <- import1 %>%
     SD = ifelse(partynameDE == "SD", partei_staerke, 0),
     SP = ifelse(partynameDE == "SP", partei_staerke, 0),
     SVP = ifelse(partynameDE == "SVP", partei_staerke, 0),
-    Uebrige = ifelse(partynameDE == "Übrige", partei_staerke, 0)
+    Uebrige = ifelse(partynameDE == "Übrige", partei_staerke, 0),
+    CSP_19 = ifelse(partynameDE == "CSP", letzte_wahl_partei_staerke, 0),
+    EDU_19 = ifelse(partynameDE == "EDU", letzte_wahl_partei_staerke, 0),
+    EVP_19 = ifelse(partynameDE == "EVP", letzte_wahl_partei_staerke, 0),
+    FDP_19 = ifelse(partynameDE == "FDP", letzte_wahl_partei_staerke, 0),
+    FGA_19 = ifelse(partynameDE == "FGA", letzte_wahl_partei_staerke, 0),
+    GLP_19 = ifelse(partynameDE == "GLP", letzte_wahl_partei_staerke, 0),
+    GRUENE_19 = ifelse(partynameDE == "GRÜNE", letzte_wahl_partei_staerke, 0),
+    Lega_19 = ifelse(partynameDE == "Lega", letzte_wahl_partei_staerke, 0),
+    LPS_19 = ifelse(partynameDE == "LPS", letzte_wahl_partei_staerke, 0),
+    MCR_19 = ifelse(partynameDE == "MCR", letzte_wahl_partei_staerke, 0),
+    Mitte_19 = ifelse(partynameDE == "Mitte", letzte_wahl_partei_staerke, 0),
+    PdA_Sol_19 = ifelse(partynameDE == "PdA/Sol.", letzte_wahl_partei_staerke, 0),
+    SD_19 = ifelse(partynameDE == "SD", letzte_wahl_partei_staerke, 0),
+    SP_19 = ifelse(partynameDE == "SP", letzte_wahl_partei_staerke, 0),
+    SVP_19 = ifelse(partynameDE == "SVP", letzte_wahl_partei_staerke, 0),
+    Uebrige_19 = ifelse(partynameDE == "Übrige", letzte_wahl_partei_staerke, 0)
   ) %>%
-  select(-partynameDE , -partynameFR, -partei_staerke) %>%
+  select(-partynameDE , -partynameFR, -partei_staerke, -letzte_wahl_partei_staerke) %>%
   group_by(municipality, municipalityId) %>% 
-  dplyr::summarize(across(CSP:Uebrige, ~ sum(.x, na.rm = TRUE)), .groups = "drop") %>% 
+  dplyr::summarize(across(CSP:Uebrige_19, ~ sum(.x, na.rm = TRUE)), .groups = "drop") %>% 
   arrange(municipalityId) 
 
 election2023 <- election2023 %>%
   mutate(municipalityId = str_pad(as.character(municipalityId), width = 4, side = "left", pad = "0"))
 
 election2023
+
 
 ### DATASET 2: Population Numbers (Swiss vs. Non-Swiss Population)
 
